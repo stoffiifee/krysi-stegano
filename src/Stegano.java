0be77f4c65f876256948d3036dd5e9c9e2fb0f4e
Sophie <sophiemarie.ordelman@students.fhnw.ch>
2019-05-25 15:28:21 +0200

    Class Stegano -importImage -getScale

--- src/Stegano.java
+++ src/Stegano.java
@@ -1,8 +1,39 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+
 public class Stegano {
+
 
     public static void main(String[] args) {
 
+    BufferedImage image1 = null;
+    BufferedImage image2 = null;
 
+
+    try{
+        image1 = ImageIO.read(new File("XXX"));
+        image2 = ImageIO.read(new File("YYY"));
+
+    } catch (IOException noImage) {
+        System.out.println("Bild konnte nicht geladen werden.");
+    }
+
+    }
+
+       public void getScale(BufferedImage image) {
+
+        int widht = image.getWidth();
+        int height = image.getHeight();
+
+
+    }
+
+    public void testSameScale(){
+        if()
 
     }
 
